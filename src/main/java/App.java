8771275a963220506bc38231b446142cd67d2cc7
Ivaneskins <weina2004@mail.ru>
2023-07-29 12:08:16 +0500

    Done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = (Cat)applicationContext.getBean("cat");
+
+        System.out.println("Hello world1 == hello world2 " + (bean == bean2));
+        System.out.println("Cat 1 == cat2 " + (cat1 == cat2));
+
     }
 }
